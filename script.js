425fc8ff1e94538390541b040d1d6cbe4255a82f
Ali Jan <alijaan.web@gmail.com>
2024-05-12 23:51:23 +0500

    completed

--- script.js
+++ script.js
@@ -2,45 +2,74 @@ const accessKey = "vXMhwy3zNvAHaA7RMTXWvGOy6K-8HosLzA-Ep6CKBuU";
 const searchForm = document.querySelector('form');
 const imagesContainer = document.querySelector('.images-container');
 const searchInput = document.querySelector('.search-input');
+const lodeMoreBtn = document.querySelector('.lodeMoreBtn');
+
+let page = 1;
 
 //Function to Fetch Images using Unsplish API
-const fetchImages = async (query) => {
-    imagesContainer.innerHTML = "";
-   const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&client_id=${accessKey}`
+const fetchImages = async (query, pageNo) => {
+    try {
 
-   const response = await fetch(url);
-   const data = await response.json();
+    if (pageNo === 1) {
+        imagesContainer.innerHTML = "";
+    }
 
-//console.log(data);
+    const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`
 
-// Creating Image Div
-data.results.forEach(photo => {
-    const imageElement = document.createElement('div');
-    imageElement.classList.add('imageDiv');
-    imageElement.innerHTML = `<img src="${photo.urls.regular}"/>`;
+    const response = await fetch(url);
+    const data = await response.json();
 
-//Creating Overlay
-    const overlayElement = document.createElement('div');
-    overlayElement.classList.add('overlay');
+if(data.results.length > 0){
+    
+    // Creating Image Div
+    data.results.forEach(photo => {
+        const imageElement = document.createElement('div');
+        imageElement.classList.add('imageDiv');
+        imageElement.innerHTML = `<img src="${photo.urls.regular}"/>`;
 
-// Creating Overlayb Text
-    const overlayText = document.createElement('h3');
-    overlayText.innerText = `${photo.alt_description}`
+        //Creating Overlay
+        const overlayElement = document.createElement('div');
+        overlayElement.classList.add('overlay');
 
-    imageElement.appendChild(overlayElement);
-    imagesContainer.appendChild(imageElement);
-    overlayElement.appendChild(overlayText)
-});
+        // Creating Overlayb Text
+        const overlayText = document.createElement('h3');
+        overlayText.innerText = `${photo.alt_description}`
+
+        imageElement.appendChild(overlayElement);
+        imagesContainer.appendChild(imageElement);
+        overlayElement.appendChild(overlayText)
+    });
+
+    if (data.total_pages === pageNo) {
+        lodeMoreBtn.style.display = "none";
+    } else {
+        lodeMoreBtn.style.display = "block";
+    }
 }
-
+else {
+    imagesContainer.innerHTML = `<h2>No Image Found</h2>`
+}
+} catch (error) {
+    imagesContainer.innerHTML = `<h2>Failed to Fetch Images. Please try again later</h2>`
+}
+}
 
 // Adding Event Listener to Search Form
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const inputText = searchInput.value.trim();
-    if(inputText !== ""){
-        fetchImages(inputText)
-    }else {
+    if (inputText !== "") {
+        page = 1;
+        fetchImages(inputText, page)
+    } else {
         imagesContainer.innerHTML = `<h2>Please enter a  search query.</h2>`
+        if(lodeMoreBtn.style.display === "block"){
+              lodeMoreBtn.style.display = "none";
+        }
     }
+})
+
+// Adding Event Listener Lode more button to fetch more Images
+lodeMoreBtn.addEventListener('click', () => {
+    fetchImages(searchInput.value.trim(), ++page);
 })
